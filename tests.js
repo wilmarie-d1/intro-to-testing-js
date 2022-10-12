5ed0eca9b712d646f8038075a7152471d2004fe7
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-10-12 13:56:31 -0500

    starting work

--- tests.js
+++ tests.js
@@ -13,3 +13,13 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+//Add a describe, an it, and an expect to assert that sayHello is a defined function. Use your tests for helloWorld as a guide.
+describe('sayHello',function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe("function");
+    });
+})
+//  Step 2: Run all the tests. At this point, we're expecting and hoping for a single, red failing test that we just now authored.
+// Step 3: Now, let's go to code.js and create an empty function definition for sayHello.
+// Step 4: Run all the tests. We're expecting all tests, including the new test for sayHello to be green.
+// Step 5: Given that this is our first (tiny) test and our first implementation, there is not yet the opportunity to refactor.
